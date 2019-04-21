5f6f3165fcdf895b34f1e56a575834f0b35b313b
TimeBye <zhongziling@vip.qq.com>
2019-04-21 11:22:32 +0800

    [FIX]修复centos内核更新

--- init-centos-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum install epel-release -y 
-sudo yum install git python36 sshpass -y
-sudo python3.6 -m ensurepip
-sudo ln -s /usr/local/bin/pip3 /usr/bin/pip3
-sudo pip3 install --no-cache-dir ansible==2.7.5 netaddr -i https://mirrors.aliyun.com/pypi/simple/
--- init-ubuntu-ansible.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install git python3-pip sshpass -y
-sudo pip3 install --no-cache-dir ansible==2.7.5 netaddr -i https://mirrors.aliyun.com/pypi/simple/
