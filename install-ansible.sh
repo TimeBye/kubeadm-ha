5cc638ad069dbc9c27570979dd1c3410d755082c
TimeBye <zhongziling@vip.qq.com>
2019-04-22 21:16:38 +0800

    [IMP]升级内核

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install epel-release -y || true
-sudo yum install git python36 sshpass -y || sudo apt-get install git python3-pip sshpass -y
+sudo yum install git python36 sshpass gcc libffi-devel python-devel openssl-devel -y || sudo apt-get install git python3-pip sshpass build-essential libssl-dev libffi-dev python-dev -y
 sudo python3.6 -m ensurepip || true
 sudo ln -s /usr/local/bin/pip3 /usr/bin/pip3 || true
 sudo pip3 install --no-cache-dir ansible==2.7.5 netaddr -i https://mirrors.aliyun.com/pypi/simple/
