573b42d43ce8613729439019155e4bfe782fa2e5
TimeBye <zhongziling@vip.qq.com>
2019-10-09 23:41:51 +0800

    [IMP]更新pip3安装方式

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo yum install epel-release -y || true
-sudo yum install git python36 sshpass -y || sudo apt-get install git python3-pip sshpass build-essential libssl-dev libffi-dev python-dev -y
-sudo python3.6 -m ensurepip || true
-sudo ln -s /usr/local/bin/pip3 /usr/bin/pip3 || true
+sudo yum install -y epel-release || sudo apt-get update
+sudo yum install -y git python3-pip sshpass || sudo apt-get install -y git python3-pip sshpass build-essential libssl-dev libffi-dev python-dev
 sudo pip3 install --no-cache-dir ansible==2.8.3 netaddr==0.7.19 -i https://mirrors.aliyun.com/pypi/simple/
-sudo ln -s /usr/local/bin/ansible /usr/bin/ansible || true
