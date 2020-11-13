d405422d1930449cbaaced87bb1ac3b01d98e00a
TimeBye <setzero@aliyun.com>
2020-11-13 11:34:24 +0800

    [FIX]修复calicoctl运行命令

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo yum install -y epel-release || sudo apt-get update
-sudo yum install -y git python3-pip sshpass || sudo apt-get install -y git python3-pip sshpass build-essential libssl-dev libffi-dev python-dev
+sudo yum install -y git python3-pip sshpass libffi-devel python3-devel openssl-devel || sudo apt-get install -y git python3-pip sshpass build-essential libssl-dev libffi-dev python3-dev
 sudo pip3 install --no-cache-dir ansible==2.8.5 netaddr==0.7.19 -i https://mirrors.aliyun.com/pypi/simple/
