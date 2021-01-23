ddfab5bf096ae38de98fd21afa3ffd857a930a23
TimeBye <setzero@aliyun.com>
2021-01-23 18:36:59 +0800

    [IMP]更新sshpass版本至1.06

--- docs/homebrew-core/sshpass.rb
+++ docs/homebrew-core/sshpass.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'https://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz'
+  url 'https://sourceforge.net/projects/sshpass/files/sshpass/1.07/sshpass-1.07.tar.gz'
   homepage 'https://sourceforge.net/projects/sshpass'
-  sha256 'c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60'
+  sha256 '986973c8dd5d75ff0febde6c05c76c6d2b5c4269ec233e5518f14f0fd4e4aaef'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
