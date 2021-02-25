057573384d7b58d31b0cbc798b375c0349bdf64d
TimeBye <setzero@aliyun.com>
2021-02-25 18:22:14 +0800

    [IMP]升级pip

--- ansible/install.sh
+++ ansible/install.sh
@@ -29,12 +29,14 @@ do_install() {
     ubuntu|debian|raspbian)
         sudo apt-get update
         sudo apt-get install -y git python3-pip sshpass build-essential libssl-dev libffi-dev python3-dev
+        sudo pip3 install -U pip -i https://mirrors.aliyun.com/pypi/simple/
         sudo pip3 install --no-cache-dir ansible==2.10.4 netaddr -i https://mirrors.aliyun.com/pypi/simple/
         exit 0
       ;;
     centos|fedora|rhel)
         sudo curl -sSLo /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
         sudo yum install -y git python3-pip sshpass libffi-devel python3-devel openssl-devel
+        sudo pip3 install -U pip -i https://mirrors.aliyun.com/pypi/simple/
         sudo pip3 install --no-cache-dir ansible==2.10.4 netaddr -i https://mirrors.aliyun.com/pypi/simple/
         exit 0
       ;;
