483504481301038bda7f1a5c68ae81b7ccfd1952
TimeBye <setzero@aliyun.com>
2021-04-25 16:16:35 +0800

    [IMP]更新 kubernetes 默认安装版本为 1.20.6

--- offline/download-yum.sh
+++ offline/download-yum.sh
@@ -62,11 +62,11 @@ repotrack net-tools
 repotrack libseccomp
 repotrack conntrack-tools
 repotrack bash-completion
-repotrack kubeadm-1.20.4
-repotrack kubectl-1.20.4
-repotrack kubelet-1.20.4
-yumdownloader --resolve kubeadm-1.20.4
-yumdownloader --resolve kubectl-1.20.4
-yumdownloader --resolve kubelet-1.20.4
+repotrack kubeadm-1.20.6
+repotrack kubectl-1.20.6
+repotrack kubelet-1.20.6
+yumdownloader --resolve kubeadm-1.20.6
+yumdownloader --resolve kubectl-1.20.6
+yumdownloader --resolve kubelet-1.20.6
 
 createrepo --update ./
