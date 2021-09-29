7e7ac80d24ec85ec3c15c5b7cd50d7d66576d31a
TimeBye <setzero@aliyun.com>
2021-09-29 15:23:10 +0800

    [IMP]更新kubernetes默认安装版本为1.22.2

--- offline/download-images-to-registry.sh
+++ offline/download-images-to-registry.sh
@@ -22,15 +22,15 @@ quay.io/coreos/flannel:v0.14.0
 quay.io/jetstack/cert-manager-cainjector:v1.4.0
 quay.io/jetstack/cert-manager-webhook:v1.4.0
 quay.io/jetstack/cert-manager-controller:v1.4.0
-k8s.gcr.io/kube-apiserver:v1.21.4
-k8s.gcr.io/kube-controller-manager:v1.21.4
-k8s.gcr.io/kube-scheduler:v1.21.4
-k8s.gcr.io/kube-proxy:v1.21.4
-k8s.gcr.io/pause:3.4.1
-k8s.gcr.io/etcd:3.4.13-0
-k8s.gcr.io/coredns/coredns:v1.8.0
+k8s.gcr.io/kube-apiserver:v1.22.2
+k8s.gcr.io/kube-controller-manager:v1.22.2
+k8s.gcr.io/kube-scheduler:v1.22.2
+k8s.gcr.io/kube-proxy:v1.22.2
+k8s.gcr.io/pause:3.5
+k8s.gcr.io/etcd:3.5.0-0
+k8s.gcr.io/coredns/coredns:v1.8.4
 k8s.gcr.io/ingress-nginx/controller:v0.47.0
-k8s.gcr.io/metrics-server/metrics-server:v0.5.0
+k8s.gcr.io/metrics-server/metrics-server:v0.5.1
 "
 
 dest_registry=${dest_registry:-'127.0.0.1:5000/kubeadm-ha'}
