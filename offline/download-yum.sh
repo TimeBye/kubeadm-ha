31309492fa497f5eae29d75fe39f71c7b0e2d71b
TimeBye <setzero@aliyun.com>
2022-12-08 14:55:44 +0800

    [IMP]更新kubernetes安装版本至1.23.14

--- offline/download-yum.sh
+++ offline/download-yum.sh
@@ -28,16 +28,16 @@ EOF
 repotrack lvm2
 repotrack audit
 repotrack device-mapper-persistent-data
-repotrack docker-ce-20.10.11
-repotrack docker-ce-cli-20.10.11
-repotrack containerd.io-1.4.6
-yumdownloader --resolve docker-ce-20.10.11
-yumdownloader --resolve docker-ce-cli-20.10.11
-yumdownloader --resolve containerd.io-1.4.6
+repotrack docker-ce-20.10.21
+repotrack docker-ce-cli-20.10.21
+repotrack containerd.io-1.6.10
+yumdownloader --resolve docker-ce-20.10.21
+yumdownloader --resolve docker-ce-cli-20.10.21
+yumdownloader --resolve containerd.io-1.6.10
 
 createrepo --update ./
-tar -czvf docker-ce-20.10.11.tar.gz *.rpm repodata
-mv docker-ce-20.10.11.tar.gz ..
+tar -czvf docker-ce-20.10.21.tar.gz *.rpm repodata
+mv docker-ce-20.10.21.tar.gz ..
 
 if [ $(uname -m) == 'x86_64' ];then
   curl -o kernel-lt-5.4.92-1.el7.elrepo.x86_64.rpm \
@@ -64,11 +64,13 @@ repotrack net-tools
 repotrack libseccomp
 repotrack conntrack-tools
 repotrack bash-completion
-repotrack kubeadm-1.23.12
-repotrack kubectl-1.23.12
-repotrack kubelet-1.23.12
-yumdownloader --resolve kubeadm-1.23.12
-yumdownloader --resolve kubectl-1.23.12
-yumdownloader --resolve kubelet-1.23.12
+repotrack kubeadm-1.23.14
+repotrack kubectl-1.23.14
+repotrack kubelet-1.23.14
+repotrack kubernetes-cni-0.8.7
+yumdownloader --resolve kubeadm-1.23.14
+yumdownloader --resolve kubectl-1.23.14
+yumdownloader --resolve kubelet-1.23.14
+yumdownloader --resolve kubernetes-cni-0.8.7
 
 createrepo --update ./
