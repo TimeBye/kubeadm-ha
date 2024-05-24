72365f195d2c50d543a5716d136b5de8bfd353c5
TimeBye <setzero@aliyun.com>
2024-05-24 17:09:55 +0800

    [IMP]更新metrics-server至0.7.1

--- offline/download-images.sh
+++ offline/download-images.sh
@@ -29,7 +29,7 @@ registry.k8s.io/etcd:3.5.12-0
 registry.k8s.io/coredns/coredns:v1.10.1
 registry.k8s.io/ingress-nginx/controller:v1.10.1
 registry.k8s.io/ingress-nginx/kube-webhook-certgen:v1.4.1
-registry.k8s.io/metrics-server/metrics-server:v0.6.4
+registry.k8s.io/metrics-server/metrics-server:v0.7.1
 "
 
 dest_registry=${dest_registry:-'127.0.0.1:5000/kubeadm-ha'}
