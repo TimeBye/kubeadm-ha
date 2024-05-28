5e1701a042276d30d45e3d8d112c36b6a25e122d
TimeBye <setzero@aliyun.com>
2024-05-28 15:54:36 +0800

    [IMP]修复calicoctl启动命令

--- offline/download-deb.sh
+++ offline/download-deb.sh
@@ -20,7 +20,7 @@ set -eux;
     tee /etc/apt/sources.list.d/docker.list > /dev/null
   
   curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
   
   apt-get update
   
