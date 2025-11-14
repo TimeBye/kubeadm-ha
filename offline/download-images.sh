ffee55dd5a65a004523e193bf3686d03e8adf5f5
TimeBye <setzero@aliyun.com>
2025-11-14 16:46:46 +0800

    [FIX]修复arm架构keepalived镜像 #162

--- offline/download-images.sh
+++ offline/download-images.sh
@@ -5,7 +5,7 @@ images="
 nginx:1.25-alpine
 haproxy:2.8-alpine
 traefik:v3.0.0
-setzero/osixia_keepalived:2.0.20
+linkvt/osixia_keepalived:stable
 setzero/chrony:3.5
 calico/typha:v3.28.2
 calico/cni:v3.28.2
